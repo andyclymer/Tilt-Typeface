74058ad5d014446bd6e261fbe0af158c345a3651
Andy Clymer <hello@andyclymer.com>
2019-12-16 14:36:51 -0500

    Small kerning adjustments and a new build for proofing

--- sources/Tilt Prism/KERNING NOTE.md
+++ sources/Tilt Prism/KERNING NOTE.md
@@ -1,3 +1,8 @@
+1) √ Copy Neon model kerning into masters
+1) Re-rotate Prism (fixed the k SB)
+2) New builds and new proofs for all styles
+
+
 # Warp
 - Fixed a HH kern in Warp, propegate it back into the sources and rebuild. Double check the kern in Neon
 
--- tools/Generate Fonts/FontInfo.py
+++ tools/Generate Fonts/FontInfo.py
@@ -80,7 +80,7 @@ def setFontInfo(f, familyName, styleName, version=(0, 0), versionString=""):
     
 
 
-sourceFolderPath = "/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Neon/Rotated Shadow"
+sourceFolderPath = "/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Warp/Rotated Shadow"
 for fileName in os.listdir(sourceFolderPath):
     if fileName.endswith(".ufo"):
         ufoPath = os.path.join(sourceFolderPath, fileName)
@@ -88,10 +88,10 @@ for fileName in os.listdir(sourceFolderPath):
         print(fileName)
         f = OpenFont(ufoPath, showInterface=False)
         
-        familyName = "Tilt Beta Neon Shadow"
+        familyName = "Tilt Beta Warp Shadow"
         styleName = "Regular"
-        version = (0, 4)
-        versionString = "BETA 2019_12_12"
+        version = (0, 5)
+        versionString = "BETA 2019_12_16"
         
         setFontInfo(f, familyName, styleName, version=version, versionString=versionString)
         
