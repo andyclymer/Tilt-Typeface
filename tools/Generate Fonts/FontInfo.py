e54d32c31c4fe2c5c6cc282d47db93dfcefbd49d
Andy Clymer <hello@andyclymer.com>
2020-02-06 15:42:08 -0500

    Re-run of the Warp, fresh build of everything (looking good)

--- tools/Generate Fonts/FontInfo.py
+++ tools/Generate Fonts/FontInfo.py
@@ -80,7 +80,7 @@ def setFontInfo(f, familyName, styleName, version=(0, 0), versionString=""):
     
 
 
-sourceFolderPath = "/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Prism/Rotated 03 Combined"
+sourceFolderPath = "/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Warp/Rotated"
 for fileName in os.listdir(sourceFolderPath):
     if fileName.endswith(".ufo"):
         ufoPath = os.path.join(sourceFolderPath, fileName)
@@ -88,7 +88,7 @@ for fileName in os.listdir(sourceFolderPath):
         print(fileName)
         f = OpenFont(ufoPath, showInterface=False)
         
-        familyName = "Tilt Beta Prism"
+        familyName = "Tilt Beta Warp"
         styleName = "Regular"
         version = (0, 7)
         versionString = "BETA 2020_02_06"
